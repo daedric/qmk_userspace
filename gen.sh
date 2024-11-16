78bbc33dd852ea9b68747a8022cf47d995d08ffb
Thomas Sanchez <me@thomas-sanchez.me>
2024-11-16 10:30:27 +0100

    fix: path

--- gen.sh
+++ gen.sh
@@ -1,3 +1,3 @@
 python scripts/gen_keymap.py  --keymap daedric --autoshift  | clang-format | tee keyboards/zsa/moonlander/keymaps/daedric/keymap.c
 python scripts/gen_keymap.py  | clang-format | tee keyboards/zsa/moonlander/keymaps/ergol/keymap.c
-python scripts/gen_keymap.py --host fr | clang-format | tee keyboards/zsa/moonlander/keymaps/ergol-fr/keymap.c
+python scripts/gen_keymap.py --host fr | clang-format | tee keyboards/zsa/moonlander/keymaps/ergolfr/keymap.c
